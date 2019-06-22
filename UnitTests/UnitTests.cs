2db37fa643eb802e4cf78957226cbeaebb505df1
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-06-22 11:43:04 +0200

    <CopyToOutputDirectory>Always</CopyToOutputDirectory>

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -1,8 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+
 using Xunit;
 using GlitchedPolygons.RepositoryPattern.SQLite;
 
